34dd2c5e3cabb5415dd72c7d95ff03830c38b2e5
alexw-gsct <80015046+alexchwong@users.noreply.github.com>
2021-05-07 16:08:49 +1000

    Update IRFinder.cpp

--- src/IRFinder.cpp
+++ src/IRFinder.cpp
@@ -678,7 +678,7 @@ int IRF_main_multithreaded(std::string reference_file, StringVector bam_files, S
 		covFile outCOV;
 		outCOV.SetOutputHandle(&ofCOV);
 		
-		oFragMap.sort_and_collapse_final(verbose);
+		oFragMap.sort_and_collapse_final(false);
 		oFragMap.WriteBinary(&outCOV, BB.chr_names, BB.chr_lens);
 		ofCOV.close();		
 	}
