990a6c76574a6de111c92192d9a9f45c0076d8cd
alexchwong <80015046+alexchwong@users.noreply.github.com>
2021-05-15 17:10:27 +1000

    Try using pointers in reference generation
    Former-commit-id: 1c68e6c2cbfddcb42740054cece0b2d7114daa8f

--- src/ReadBlockProcessor_CoverageBlocks.h
+++ src/ReadBlockProcessor_CoverageBlocks.h
@@ -33,6 +33,7 @@ class CoverageBlocks : public ReadBlockProcessor {
 		std::vector<BEDrecord> BEDrecords;
 
 	public:
+    ~CoverageBlocks() {};
 		void ProcessBlocks(const FragmentBlocks &fragblock);
 		void ChrMapUpdate(const std::vector<chr_entry> &chrmap);
 		void loadRef(std::istringstream &IN);
@@ -51,6 +52,7 @@ class CoverageBlocks : public ReadBlockProcessor {
 
 class CoverageBlocksIRFinder : public CoverageBlocks {
 	public:
+    ~CoverageBlocksIRFinder() {};
 		int WriteOutput(std::string& output, std::string& QC, const JunctionCount &JC, const SpansPoint &SP, const FragmentsMap &FM, int directionality = 0) const;
 };
 
