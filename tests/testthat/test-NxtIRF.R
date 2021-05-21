208f7a937e32f1d050f6b32ce9f1d037cc22df45
alexchwong <80015046+alexchwong@users.noreply.github.com>
2021-05-21 13:37:47 +1000

    Fixed situation where IS_EOB is not set as the first block to read is EOF
    Former-commit-id: 8aeced63701a7d4d653f5f04fd407382740cad2d

--- tests/testthat/test-NxtIRF.R
+++ tests/testthat/test-NxtIRF.R
@@ -5,7 +5,7 @@ test_that("NxtIRF pipeline reproduces NxtSE object", {
         reference_path = file.path(tempdir(), "Reference")
     )
     
-    # i = 1 
+    # i = 2
     # NxtIRF:::.irfinder_run_single(bams$BAM[i], 
         # file.path(tempdir(), "Reference", "IRFinder.ref.gz"), 
         # file.path(tempdir(),"Output"), TRUE, TRUE, n_threads = 2
@@ -14,7 +14,7 @@ test_that("NxtIRF pipeline reproduces NxtSE object", {
     IRFinder(bams$BAM, bams$sample,
         reference_path = file.path(tempdir(), "Reference"),
         output_path = file.path(tempdir(), "IRFinder_output"),
-        overwrite = TRUE, n_threads = 1
+        overwrite = TRUE, n_threads = 1, verbose = TRUE
     )
     expr <- Find_IRFinder_Output(file.path(tempdir(), "IRFinder_output"))
     CollateData(expr, 
