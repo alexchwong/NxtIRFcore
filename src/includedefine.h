4f8d514e83dd5423b20340ec184270b7b3e29bdd
alexchwong <80015046+alexchwong@users.noreply.github.com>
2021-09-20 11:08:11 +1000

    Added / updated copyright notices to source code
    Former-commit-id: e0219cdf9898b54088df8cc3d08eb9e0e4e5dfd9

--- src/includedefine.h
+++ src/includedefine.h
@@ -1,7 +1,29 @@
+/* includedefine.h Shared includes and declarations for IRFinder/NxtIRF
+
+Copyright (C) 2021 Alex Chit Hei Wong
+Copyright (C) 2016 William Ritchie
+  - original: https://github.com/williamritchie/IRFinder/tree/IRFinder-1.3.1)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.  */
+
 #ifndef INCLUDEDEFINE_DEF
 #define INCLUDEDEFINE_DEF
-
-// #pragma GCC diagnostic ignored "-Wpedantic"
 
 #include <cstring>
 #include <time.h>
@@ -16,17 +38,18 @@
 #include <algorithm> // std::sort
 #include <functional> // std::function
 #include <stdexcept>
-#include <zlib.h>
-#include <zconf.h>
 #include <math.h>
 #include <chrono>
 #include <thread>
+
+// [[Rcpp::depends(zlibbioc)]]
+#include <zlib.h>
+#include <zconf.h>
 
 using namespace std;
 
 // Declare GALAXY in make file so that these sources can be compiled into
 // executable file from within Galaxy / Linux
-
 #ifndef GALAXY
   #include <Rcpp.h>
   using namespace Rcpp;
