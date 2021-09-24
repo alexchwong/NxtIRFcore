ce25fbe3e36d2edcfd332369b9a40c64cf671379
alexchwong <80015046+alexchwong@users.noreply.github.com>
2021-09-25 09:39:39 +1000

    Disable BiocCheck for remotes
    Also reorganise AllImports.R
    
    
    Former-commit-id: 230b1763bdc776b2478ee3a41ae58557ce90c6c5

--- R/AllImports.R
+++ R/AllImports.R
@@ -1,12 +1,13 @@
 #' @useDynLib NxtIRFcore, .registration = TRUE
 #' @import NxtIRFdata
 #' @importFrom methods as is coerce callNextMethod new
+#' @importFrom parallel detectCores
 #' @importFrom stats as.formula model.matrix qt runif na.omit prcomp
 #' @importFrom utils download.file packageVersion getFromNamespace
 #' @importFrom utils setTxtProgressBar txtProgressBar
-#' @importFrom R.utils gzip
-#' @importFrom magrittr %>%
 #' @importFrom tools R_user_dir
+#' @importFrom magrittr %>%
+#' @importFrom R.utils gzip
 #' @importFrom Rcpp evalCpp
 #' @import data.table
 #' @importFrom fst read.fst write.fst
@@ -32,28 +33,31 @@
 #' @importFrom GenomicRanges strand<- seqnames<-
 #' @importFrom HDF5Array HDF5Array writeHDF5Array h5writeDimnames 
 #' @importFrom IRanges IRanges Views RleList
-#' @importFrom parallel detectCores
 #' @importFrom plotly config layout plotlyOutput event_data ggplotly 
 #' @importFrom plotly plotlyProxy plotlyProxyInvoke renderPlotly subplot 
 #' @importFrom plotly highlight
 #' @importFrom rhdf5 h5createFile h5createDataset h5delete h5write h5createGroup
 #' @importFrom rtracklayer import export TwoBitFile track
+#' @importFrom S4Vectors metadata Rle metadata<- SimpleList 
+#' @importFrom S4Vectors from to setValidity2 DataFrame
+#' @importFrom S4Vectors bindCOLS bindROWS getListElement setListElement
 #' @importFrom SummarizedExperiment SummarizedExperiment 
 #' @importFrom SummarizedExperiment rowData colData rowData<- colData<-
 #' @importFrom SummarizedExperiment assay assays assay<- assays<-
 #' @importFrom SummarizedExperiment assayNames assayNames<-
-#' @importFrom S4Vectors metadata Rle metadata<- SimpleList 
-#' @importFrom S4Vectors from to setValidity2 DataFrame
-#' @importFrom S4Vectors bindCOLS bindROWS getListElement setListElement
-#' @importClassesFrom SummarizedExperiment SummarizedExperiment
 #' @importClassesFrom S4Vectors DataFrame
+#' @importClassesFrom SummarizedExperiment SummarizedExperiment
 NULL
 
-S4_disableValidity <- getFromNamespace("disableValidity", "S4Vectors")
+# Import namespaces of internal functions of BiocGenerics and S4Vectors
+# Used to set up NxtSE class
+
 BG_replaceSlots <- getFromNamespace("replaceSlots", "BiocGenerics")
-SE_charbound <- 
-    function(idx, txt, fmt)
-{
+S4_disableValidity <- getFromNamespace("disableValidity", "S4Vectors")
+S4_selectSome <- getFromNamespace("selectSome", "S4Vectors")
+
+# Checks character indices on NxtSE object
+SE_charbound <- function(idx, txt, fmt) {
     orig <- idx
     idx <- match(idx, txt)
     if (any(bad <- is.na(idx))) {
@@ -63,4 +67,3 @@ SE_charbound <-
     idx
 }
 
-S4_selectSome <- getFromNamespace("selectSome", "S4Vectors")
